8126035cf596ce80e62b5e55dbb1a8c4fbb79771
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-06-26 13:09:26 +0400

    Added Test code for API endpoint ✅ + Added Dependencies in Cargo.toml ✅

--- rustytrader/src/main.rs
+++ rustytrader/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize)]
+struct SymbolData {
+    symbol: String,
+    name: String,
+    exchange: String,
+    country: String,
+    currency: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Make sure to replace YOUR_API_KEY with your actual API key
+    let api_key = "YOUR_API_KEY";
+    let symbol = "AAPL";
+
+    // Build the API URL
+    let url = format!(
+        "https://eodhistoricaldata.com/api/symbols/{symbol}?api_token={api_key}",
+        symbol = symbol,
+        api_key = api_key
+    );
+
+    // Send the HTTP GET request
+    let response = reqwest::get(&url).await?.text().await?;
+
+    // Deserialize the JSON response
+    let symbol_data: SymbolData = serde_json::from_str(&response)?;
+
+    // Print the symbol data
+    println!("{:#?}", symbol_data);
+
+    Ok(())
 }
